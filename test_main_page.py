5e86a1a43d1fc7344bc8a741037982a65f6f1bbe
koste <basov04@mail.ru>
2025-04-02 14:48:41 +0300

    add LoginPage class

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,22 @@
 import time
+import pytest
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
-
+# @pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    # page.go_to_login_page()
+    page.should_be_login_link()
 
-
-
-
+# @pytest.mark.skip
+def test_login_register_form(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
 
     time.sleep(5)
