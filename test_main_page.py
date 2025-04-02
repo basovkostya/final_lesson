6e512915c88efb574655ece44118f7b3f3cbda74
koste <basov04@mail.ru>
2025-04-02 15:00:10 +0300

    transition between pages

--- test_main_page.py
+++ test_main_page.py
@@ -4,15 +4,17 @@ from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
-# @pytest.mark.skip
+#@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    # page.go_to_login_page()
-    page.should_be_login_link()
+    page.go_to_login_page()
+    # page.should_be_login_link()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
-# @pytest.mark.skip
+@pytest.mark.skip
 def test_login_register_form(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = LoginPage(browser, link)
