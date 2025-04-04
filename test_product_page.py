391b8ba7903e47661571eebf0d23037b52c55c1d
koste <basov04@mail.ru>
2025-04-04 18:02:39 +0300

    Finish

--- test_product_page.py
+++ test_product_page.py
@@ -21,13 +21,12 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
-    # @pytest.mark.xfail
-    def test_user_cant_see_success_message_after_adding_product_to_basket(self, browser):
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
         link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
         page = ProductPage(browser, link)
         page.open()
         page.add_product_to_basket()
-        # page.should_not_be_success_message()
 
 @pytest.mark.parametrize('offer', ['offer0', 'offer1', 'offer2', 'offer3', 'offer4', 'offer5', 'offer6', pytest.param('offer7', marks=pytest.mark.xfail), 'offer8', 'offer9'])
 def test_should_be_elements_in_page(browser, offer):
@@ -39,11 +38,6 @@ def test_should_be_elements_in_page(browser, offer):
     page.product_name_added_to_cart()
     page.message_add_product_to_basket()
     page.price_comparison()
-
-
-
-
-
 
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
@@ -59,12 +53,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasketPage(browser, link)
@@ -72,3 +68,10 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket_with_header_basket()
     page.basket_is_empty()
     page.message_basket_is_empty()
+
+@pytest.mark.need_review
+def test_quest_can_add_product_to_basket(browser):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
